870503b61f9fcc82f52c492689fa5e97e2c9de76
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-07 15:41:40 -0600

    Clase 04-01

---
+++ Semana 04/Clase 04-01.py
@@ -0,0 +1,31 @@
+usuario = "Rogelio"
+cantidad_alumnos = 79
+media_edad = 18.231234
+monto_hope = 1234567.8901
+inversion_evento = -98765.21548
+
+"""print(type(cantidad_alumnos))
+print(type(media_edad))
+
+print(type(cantidad_alumnos) is int) #no es lo mismo "int"
+print(type(media_edad) is int)"""
+
+"""print("el usuario es", usuario, "y tiene", cantidad_alumnos, "pajaritos en su aula")
+print("y la edad promedio es de", media_edad)
+
+print(f"El usuario es {usuario}")
+print(f"y en su aula con {cantidad_alumnos - 4} pajaritos")
+print(f"con edad promedio de {media_edad:.1f} años")
+print(f"colectaron ${monto_hope:,.2f} como un donativo.")
+print(f"y la totalidad de gastos fue de ${abs(inversion_evento):,.2f} dolares")   
+
+print(type(usuario) is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo) is not bool)
+print(type(monto_hope) is not bool)"""
+
+nombre = "Rogelio"
+apellido= "Rivera"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
