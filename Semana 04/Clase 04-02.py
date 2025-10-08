611380a8d108da0b8ab62a9968038245d70c385b
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-07 18:37:10 -0600

    Clase 04-02

---
+++ Semana 04/Clase 04-02.py
@@ -0,0 +1,8 @@
+cantidad_alumnos =25
+nombre_profe = "Rogelio"
+nuevas_inscripciones = 0
+nombre_profe = input("Ingrese el nombre del profesor: ")
+nuevas_inscripciones = int(input("nuevos alumnos: "))
+print(type(nombre_profe))
+print(nombre_profe)
+print(cantidad_alumnos + nuevas_inscripciones)
