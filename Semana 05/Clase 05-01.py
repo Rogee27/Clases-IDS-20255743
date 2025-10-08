24b97c00a0c4bdb8786b4ee746e7228f278e4a23
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-07 19:39:40 -0600

    clase 05

---
+++ Semana 05/Clase 05-01.py
@@ -0,0 +1,23 @@
+
+Nombre = "Rogelio"
+palabra = "RECONOCER"
+palabra2= "raton"
+print(palabra[1::2])
+print(palabra2[::-1])
+
+##########################
+
+prueba = "bob"
+
+print(prueba==prueba[::-1])
+
+numero = "1234"
+
+print(len(prueba))
+
+edad = int(input())
+#print(len(edad))
+
+print(palabra.lower())
+print(palabra2.upper())
+print(palabra.capitalize())
