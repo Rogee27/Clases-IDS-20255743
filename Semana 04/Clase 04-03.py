4cb40e51b9290652a04baf66e2093f5e25238e3e
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-08 09:10:52 -0600

    Clase 04-03

---
+++ Semana 04/Clase 04-03.py
@@ -0,0 +1,17 @@
+
+my_string = "ABCCC" # string, cadena de caracteres 
+
+"""letra = int(input("Indique el indice de la letra a mostrar (del 0 al 4): "))
+print(my_string[letra])"""
+
+#print(len(my_string))
+#print(my_string[1:5:50]), para subsetting por slices
+
+#print("O" not in my_string)
+
+#print("AJP"*8)
+
+#print(min(my_string))
+#print(max(my_string))
+
+print(my_string.count("C"))
