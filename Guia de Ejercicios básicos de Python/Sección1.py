cf856af81c3c0cec13691f9b4090354c603a7a37
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-09 13:26:14 -0600

    Sección 1

---
+++ Guia de Ejercicios básicos de Python/Sección1.py
@@ -0,0 +1,15 @@
+nombre = "Rogelio"
+print(nombre)
+edad = 19
+ciudad = "Armenia"
+profesión = "estudiante"
+print(f"Mi nombre es {nombre}, tengo {edad} años, vivo en la ciudad de {ciudad} y soy un {profesión}.")
+#Una variable, en un programa, sirve para almacenar datos.
+mensaje = "Bienvenido al curso de Python"
+print(mensaje)
+anie_actual = 2025
+anio_nacimiento = 2006
+print(anie_actual - anio_nacimiento)
+pi = 3.14159265359
+print(f"El valor aproximado de pi es {pi:.5}")
+#La variable tipo entero se colocan solo números y la tipo cadena es para texto y se coloca entre comillas.
