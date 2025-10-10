f4399f4a18e500302938bbbc2ada3225159503a7
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-09 19:41:56 -0600

    control2

---
+++ Guía Control/GuíaControl2.py
@@ -0,0 +1,82 @@
+#1
+
+"""Enero = int(input("¿Cuánto produció en enero?:"))
+Febreo = int(input("¿Cuánto produció en febrero?:"))
+Marzo = int(input("¿Cuánto produció en marzo?:"))
+
+print(f"El costo total es de: ${(Enero*1.25) + (Febreo*1.38) + (Marzo*1.14)}")"""
+
+#2
+
+"""Dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
+
+Cantidades_lunes = int(input())
+Dias[0] = Cantidades_lunes
+
+Cantidades_martes = int(input())
+Dias[1] = Cantidades_martes
+
+Cantidades_miercoles = int(input())
+Dias[2] = Cantidades_miercoles 
+
+Cantidades_jueves = int(input())
+Dias[3] = Cantidades_jueves
+
+Cantidades_viernes = int(input())
+Dias[4] = Cantidades_viernes
+
+print(Dias)"""
+
+#3
+"""Frutas_favoritas = ["uno", "dos", "tres"]
+#niño 1
+ninio = int(input()) #El numero de niño
+fruta = input()
+
+Frutas_favoritas[ninio] = fruta
+print(Frutas_favoritas)
+#niño 2
+ninio2 = int(input()) 
+fruta = input()
+
+Frutas_favoritas[ninio2] = fruta
+print(Frutas_favoritas)
+#niño3
+ninio3 = int(input()) 
+fruta = input()
+
+Frutas_favoritas[ninio3] = fruta
+print(Frutas_favoritas)"""
+
+#4
+
+"""alumnos = ["Juan", "Ana", "Pedro"]
+
+orden = int(input("Orden en el que entraste(1-3): "))
+
+print(f"El niño que entro en orden {orden} se llama: {alumnos[orden-1]}")"""
+
+#5
+
+"""nombre = input("nombre: ")
+apellido = input("apellido: ")
+
+print(f"{nombre.lower()}.{apellido.lower()}@ISND.com")
+print(f"{nombre.lower()[0]}{apellido.lower()}@ISND.com")"""
+
+#6
+
+salario = input("Ingrese su salario: ")
+
+print(salario[0]=="$")
+print(salario.count("$")==1)
+
+
+
+
+
+
+
+
+
+
