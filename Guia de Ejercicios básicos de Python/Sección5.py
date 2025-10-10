888797f553128ed9935f7808762f61bd28e38d4e
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-10 09:52:36 -0600

    Sección 5

---
+++ Guia de Ejercicios básicos de Python/Sección5.py
@@ -0,0 +1,22 @@
+"""palabra = "Python"
+print(palabra[0])
+print(palabra[0:3])
+print(palabra[-2:])"""
+
+"""frase = "Aprender Python es divertido"
+print(frase[9:15])"""
+
+"""palabra = input("Dime una palabra: ")
+print(f"Su palabra tiene: {len(palabra)} letras")"""
+
+"""palabra = input("Dime una palabra: ")
+print(f"La primera letra es: {palabra[0]} y su última es: {palabra[-1:]}")"""
+
+"""codigo = "ABC1234XYZ"
+print(codigo[3:7])"""""
+
+nombre = "Juan Pérez"
+print(nombre[5:10])
+
+
+
