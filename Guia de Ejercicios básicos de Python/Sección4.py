92a16db5abe45a45770dae59d7f0ca399a15b23b
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-10 09:26:08 -0600

    Sección 4

---
+++ Guia de Ejercicios básicos de Python/Sección4.py
@@ -0,0 +1,19 @@
+"""nombre = "Ana"
+producto = "Laptop"
+precio = 1200
+print(f"Hola {nombre}, el producto {producto} cuesta ${precio}")"""
+
+"""nombre = input("¿Cuál es su nombre?: ")
+pais = input("¿De qué país es?: ")
+print(f"Hola, {nombre} de {pais}, ¡bienvenido!.")"""
+
+"""base = float(input("Dame la medidad de la base de un rectángulo: "))
+altura = float(input("Dame la medida de la altura de un rectángulo: "))
+print(f"El área del rectángulo es de: {base*altura:.2f}")"""
+
+Producto = "Pan"
+Precio = 1.50
+Cantidad = 4
+Total = 6
+print(f"Producto: {Producto}, Precio: ${Precio}, Cantidad: {Cantidad}, Total: ${Total}")
+
