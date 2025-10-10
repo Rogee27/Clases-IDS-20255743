d607aba7764550347def19e8d01aef10b307ba8c
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-10 10:37:14 -0600

    Guia control 2

---
+++ Guía Control/OmegaupPractica.py
@@ -0,0 +1,22 @@
+Dias = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES"]
+
+Cantidades_lunes = int(input())
+Dias[0] = Cantidades_lunes
+
+Cantidades_martes = int(input())
+Dias[1] = Cantidades_martes
+
+Cantidades_miercoles = int(input())
+Dias[2] = Cantidades_miercoles 
+
+Cantidades_jueves = int(input())
+Dias[3] = Cantidades_jueves
+
+Cantidades_viernes = int(input())
+Dias[4] = Cantidades_viernes
+
+print(f"El día LUNES se vendieron {Dias[0]} productos")
+print(f"El día MARTES se vendieron {Dias[1]} productos")
+print(f"El día MIERCOLES se vendieron {Dias[2]} productos")
+print(f"El día JUEVES se vendieron {Dias[3]} productos")
+print(f"El día VIERNES se vendieron {Dias[4]} productos")
