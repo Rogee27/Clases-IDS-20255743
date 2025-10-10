073ee92643f25a286f08e2d1b0c5e72ff7c7fd59
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-10 10:07:27 -0600

    Sección 6

---
+++ Guia de Ejercicios básicos de Python/Sección6.py
@@ -0,0 +1,13 @@
+"""frase = "La programación es poderosa"
+print("poderosa" in frase)
+print("Java" not in frase)"""
+
+"""palabra = "hola"
+print(palabra*5)"""
+
+"""texto = "banana"
+print(texto.count("a"))"""
+
+mensaje = "El Salvador es un gran país"
+print(mensaje.index("gran"))
+
