e6996d782d1c8c8f1e487bb7df48445dc05d66bc
Rogee27 <rogeliorivera0627@gmail.com>
2025-10-10 08:52:30 -0600

    Sección 3

---
+++ Guia de Ejercicios básicos de Python/Sección3.py
@@ -0,0 +1,36 @@
+"""a = 10
+b = 3
+c = 5
+print(f"La suma de a y b es igual a: {a+b}")
+print(f"El resultado de a dividido entre b es: {a / b:.3}")
+print(f"El residuo de la división entre a y c es: {a % c}")
+
+numero = 7
+print(numero > 5)
+
+x = 10
+y = 10
+print(x is y)
+print(x is not y)
+
+numero_usuario = int(input("Dame un número: "))
+print(numero_usuario % 2 == 0)
+
+registrado = True
+otro = False
+print(registrado and otro)
+print(registrado or otro)
+print(not registrado)
+print(not otro)"""
+
+"""num1 = input("Dame un número: ")
+num2 = input("Dame otro número: ")
+print(num1 >= num2)"""
+
+"""edad = int(input("Dame tu edad: "))
+print(edad >= 18)"""
+
+edad = 22
+estudiante = True
+print(edad>=18 and estudiante)
+
